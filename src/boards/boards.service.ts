a411c7dc0b570f469ca0b97c0a085fc2ae2f059e
yujo <yujo.dev@gmail.com>
2022-01-09 22:27:51 +0900

    feat: exception 로직 추가

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -12,7 +12,13 @@ export class BoardsService {
   }
 
   getBoardById(id: string): Board {
-    return this.boards.find((board) => board.id === id);
+    const board = this.boards.find((board) => board.id === id);
+
+    if (!board) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+
+    return board;
   }
 
   createBoard(createBoardDto: CreateBoardDto) {
@@ -28,6 +34,7 @@ export class BoardsService {
   }
 
   deleteBoard(id: string): void {
+    this.getBoardById(id);
     this.boards = this.boards.filter((board) => board.id !== id);
   }
 
